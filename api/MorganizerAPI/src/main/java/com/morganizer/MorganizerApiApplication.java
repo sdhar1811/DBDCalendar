4ce7140a430f0feb7ef1af7cc18882f74e105d3d
Sharad Dhar <42695310+sdhar1811@users.noreply.github.com>
2021-02-05 15:16:08 -0700

    initial API code

---
+++ api/MorganizerAPI/src/main/java/com/morganizer/MorganizerApiApplication.java
@@ -0,0 +1,13 @@
+package com.morganizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MorganizerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MorganizerApiApplication.class, args);
+	}
+
+}
