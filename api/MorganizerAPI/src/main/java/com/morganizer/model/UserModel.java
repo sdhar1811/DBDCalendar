d4522bec54a1958d1d01fe3b79569178515bc8b2
khushboogithub <khushboogupta1406@gmail.com>
2021-02-12 10:58:38 -0700

    Fetch Events details from restapi endpoint

--- api/MorganizerAPI/src/main/java/com/morganizer/model/UserModel.java
+++ api/MorganizerAPI/src/main/java/com/morganizer/model/UserModel.java
@@ -3,7 +3,12 @@ package com.morganizer.model;
 public class UserModel {
 	String username;
 	String password;
-	
+	int gender;
+	String email;
+	String firstName;
+	String lastName;
+	String birthdate;
+	String phoneNumber;
 	
 	public String getUsername() {
 		return username;
@@ -17,10 +22,41 @@ public class UserModel {
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	
-	
-	
-	
-	
 
+	public int getGender() {
+		return gender;
+	}
+	public void setGender(int gender) {
+		this.gender = gender;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getBirthdate() {
+		return birthdate;
+	}
+	public void setBirthdate(String birthdate) {
+		this.birthdate = birthdate;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
 }
