63a4d30c1ef6f880b9188b1dd9cad9fa191321a4
Sharad Dhar <42695310+sdhar1811@users.noreply.github.com>
2021-02-12 19:19:35 -0700

    Task-25: updated register component apis

--- api/MorganizerAPI/src/main/java/com/morganizer/controller/UserController.java
+++ api/MorganizerAPI/src/main/java/com/morganizer/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.morganizer.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,13 +9,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.morganizer.model.UserModel;
 import com.morganizer.service.LoginService;
+import com.morganizer.service.UserSignupService;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/user")
 public class UserController {
 	
 	@Autowired
 	LoginService loginService;
+	@Autowired
+	UserSignupService signUpService;
 	
 	
 	@PostMapping("/login")
@@ -24,7 +29,10 @@ public class UserController {
 	}
 	@PostMapping("/register")
 	public void registerUser(@RequestBody UserModel userDetails) throws Exception {
-		loginService.registerUser(userDetails);
+		System.out.println("password:"+userDetails.getPassword());
+		System.out.println("Gender:"+userDetails.getGender());
+		signUpService.registerUser(userDetails);
+		
 	}
 
 }
