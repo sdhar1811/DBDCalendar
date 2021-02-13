ae213bcf63ba372531a04a9c6b2cfe0affed2bab
Satyen Verma <sverma44@asu.edu>
2021-02-12 17:14:14 -0700

    Task#16 API: Create REST API endpoint to fetch user role information

---
+++ api/MorganizerAPI/src/main/java/com/morganizer/repository/UserRolesRepository.java
@@ -0,0 +1,9 @@
+package com.morganizer.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.morganizer.entity.UserRolesEntity;
+
+public interface UserRolesRepository extends JpaRepository<UserRolesEntity, String>{
+
+}
