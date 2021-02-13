38c91d401d6c2d2320da4ba9d02b11c3f7b7a885
Sharad Dhar <42695310+sdhar1811@users.noreply.github.com>
2021-02-12 20:20:10 -0700

    Task-#25: (feat) calendar component on home screen

--- ui/MorganizerUI/src/app/app-routing.module.ts
+++ ui/MorganizerUI/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeScreenComponent } from './core/home-screen/home-screen.component';
 import { LandingComponent } from './core/landing/landing.component';
 import { LoginComponent } from './core/login/login.component';
 import { RegisterComponent } from './core/register/register.component';
@@ -8,6 +9,7 @@ const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'home', component: HomeScreenComponent },
 ];
 
 @NgModule({
