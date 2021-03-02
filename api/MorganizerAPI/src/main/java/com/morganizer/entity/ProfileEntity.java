1c6920462d1e0b2e3e9e1f08a0d13ac7c9a559ae
ddharne <ddharne@users.github.com>
2021-03-02 12:43:53 -0700

    Task#70: Created api to fetch all profile by userid.

--- api/MorganizerAPI/src/main/java/com/morganizer/entity/ProfileEntity.java
+++ api/MorganizerAPI/src/main/java/com/morganizer/entity/ProfileEntity.java
@@ -1,14 +1,14 @@
 package com.morganizer.entity;
 
-import org.apache.tomcat.jni.User;
-import org.springframework.data.annotation.Id;
-import sun.util.resources.Bundles;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
+@Entity(name="profile")
 public class ProfileEntity {
 
     @Id
@@ -19,6 +19,7 @@ public class ProfileEntity {
     private String lastName;
     private String middleName;
     private String email;
+    private String phoneNumber;
     private String gender;
     private String birthdate;
     private String color;
@@ -26,5 +27,87 @@ public class ProfileEntity {
     @ManyToOne
     @JoinColumn(name="user_id",referencedColumnName = "user_id")
     private UserDetailsEntity user;
+
+	public long getProfileId() {
+		return profileId;
+	}
+
+	public void setProfileId(long profileId) {
+		this.profileId = profileId;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getMiddleName() {
+		return middleName;
+	}
+
+	public void setMiddleName(String middleName) {
+		this.middleName = middleName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getBirthdate() {
+		return birthdate;
+	}
+
+	public void setBirthdate(String birthdate) {
+		this.birthdate = birthdate;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public UserDetailsEntity getUser() {
+		return user;
+	}
+
+	public void setUser(UserDetailsEntity user) {
+		this.user = user;
+	}
+    
+    
 
 }
