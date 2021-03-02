7ca84213eed583d23f5a0f24193631cb9d81dcfe
khushboogithub <khushboogupta1406@gmail.com>
2021-03-01 18:24:17 -0700

    Method to delete profile

--- api/MorganizerAPI/src/main/java/com/morganizer/controller/UserController.java
+++ api/MorganizerAPI/src/main/java/com/morganizer/controller/UserController.java
@@ -1,13 +1,7 @@
 package com.morganizer.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.morganizer.model.UserModel;
 import com.morganizer.service.LoginService;
@@ -42,4 +36,5 @@ public class UserController {
 	public void fetchRoles(@RequestParam String username) throws Exception {
 		userSignupService.fetchUserRole(username);
 	}
+
 }
