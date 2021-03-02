5426a7f3b0e71b414bd2f45a87ac2abe8ae86dd4
khushboogithub <khushboogupta1406@gmail.com>
2021-03-02 15:19:07 -0700

    Task#59Method to fetch Recurring Options

---
+++ api/MorganizerAPI/src/main/java/com/morganizer/repository/RecurringOptionsRepository.java
@@ -0,0 +1,9 @@
+package com.morganizer.repository;
+
+import com.morganizer.entity.RecurringOptionsEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RecurringOptionsRepository extends JpaRepository<RecurringOptionsEntity,Long> {
+
+
+}
