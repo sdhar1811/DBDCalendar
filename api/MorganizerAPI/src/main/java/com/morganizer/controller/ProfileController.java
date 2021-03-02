92985f166beea5ebc4218461c01f7b43f0a69355
khushboogithub <khushboogupta1406@gmail.com>
2021-03-01 19:58:13 -0700

    Added Entity,repository,Dto

--- api/MorganizerAPI/src/main/java/com/morganizer/controller/ProfileController.java
+++ api/MorganizerAPI/src/main/java/com/morganizer/controller/ProfileController.java
@@ -1,20 +1,23 @@
 package com.morganizer.controller;
 
+import com.morganizer.dto.ProfileRequest;
 import com.morganizer.service.ProfileService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import sun.java2d.cmm.Profile;
+
 
 public class ProfileController {
 
     @Autowired
     ProfileService profileService;
 
-    @DeleteMapping("/profilename")
-    public void deleteProfile(@RequestParam String profileId) throws Exception{
-        profileService.deleteProfile(profileId);
+    @DeleteMapping("/deleteProfile")
+    public void deleteProfile(@RequestBody ProfileRequest profileReq) {
+        profileService.deleteProfile(profileReq);
     }
 
 }
--- api/MorganizerAPI/src/main/java/com/morganizer/entity/ProfileEntity.java
+++ api/MorganizerAPI/src/main/java/com/morganizer/entity/ProfileEntity.java
@@ -1,4 +1,30 @@
 package com.morganizer.entity;
 
+import org.apache.tomcat.jni.User;
+import org.springframework.data.annotation.Id;
+import sun.util.resources.Bundles;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
 public class ProfileEntity {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private long profileId;
+
+    private String firstName;
+    private String lastName;
+    private String middleName;
+    private String email;
+    private String gender;
+    private String birthdate;
+    private String color;
+
+    @ManyToOne
+    @JoinColumn(name="user_id",referencedColumnName = "user_id")
+    private UserDetailsEntity user;
+
 }
