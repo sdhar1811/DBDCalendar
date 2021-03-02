f3d728e85d04130aaaad9d7c71283b7652e67209
ddharne <ddharne@users.github.com>
2021-03-02 11:03:51 -0700

    Task#61_62: Created DB tables for Events and Recurring mode

--- api/MorganizerAPI/src/main/java/com/morganizer/entity/EventDetailsEntity.java
+++ api/MorganizerAPI/src/main/java/com/morganizer/entity/EventDetailsEntity.java
@@ -1,5 +1,123 @@
 package com.morganizer.entity;
 
-public class EventDetailsEntity {
+import java.sql.Timestamp;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity(name = "events")
+public class EventDetailsEntity {
+	
+	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name = "event_id")
+	private long id;
+	
+	@ManyToOne
+	@JoinColumn(name = "user_id", referencedColumnName = "user_id")
+	private UserDetailsEntity user;
+	
+	private String eventTitle;
+	private String eventDescription;
+	private Timestamp startTime;
+	private Timestamp endTime;
+	
+	@ManyToOne
+	@JoinColumn(name = "mode_id", referencedColumnName = "mode_id")
+	private RecurringModeEntity recurringMode;
+	
+	private String location;
+	private String[] participant; 
+	
+	private Timestamp lastUpdatedOn;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public UserDetailsEntity getUser() {
+		return user;
+	}
+
+	public void setUser(UserDetailsEntity user) {
+		this.user = user;
+	}
+
+	public String getEventTitle() {
+		return eventTitle;
+	}
+
+	public void setEventTitle(String eventTitle) {
+		this.eventTitle = eventTitle;
+	}
+
+	public String getEventDescription() {
+		return eventDescription;
+	}
+
+	public void setEventDescription(String eventDescription) {
+		this.eventDescription = eventDescription;
+	}
+
+	public Timestamp getStartTime() {
+		return startTime;
+	}
+
+	public void setStartTime(Timestamp startTime) {
+		this.startTime = startTime;
+	}
+
+	public Timestamp getEndTime() {
+		return endTime;
+	}
+
+	public void setEndTime(Timestamp endTime) {
+		this.endTime = endTime;
+	}
+
+	public RecurringModeEntity getRecurringMode() {
+		return recurringMode;
+	}
+
+	public void setRecurringMode(RecurringModeEntity recurringMode) {
+		this.recurringMode = recurringMode;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public String[] getParticipant() {
+		return participant;
+	}
+
+	public void setParticipant(String[] participant) {
+		this.participant = participant;
+	}
+
+	public Timestamp getLastUpdatedOn() {
+		return lastUpdatedOn;
+	}
+
+	public void setLastUpdatedOn(Timestamp lastUpdatedOn) {
+		this.lastUpdatedOn = lastUpdatedOn;
+	}
+	
+	
+	
+	
+	
+	
 }
