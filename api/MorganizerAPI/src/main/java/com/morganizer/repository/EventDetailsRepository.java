e17f9f933c1e18d64c7619834b6ac6800b2e1f2a
AsmiGandhi <aagandh3@asu.edu>
2021-03-02 19:48:29 -0700

    delete event query using jparepository

--- api/MorganizerAPI/src/main/java/com/morganizer/repository/EventDetailsRepository.java
+++ api/MorganizerAPI/src/main/java/com/morganizer/repository/EventDetailsRepository.java
@@ -1,9 +1,14 @@
-//package com.morganizer.repository;
-//
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//import com.morganizer.entity.EventDetailsEntity;
-//
-//public interface EventDetailsRepository extends JpaRepository<EventDetailsEntity,Long>{
-//
-//}
+package com.morganizer.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.morganizer.entity.EventDetailsEntity;
+
+public interface EventDetailsRepository extends JpaRepository<EventDetailsEntity,Long>{
+	
+	void deleteByEventIdAndUserId(Long eventId,Long userId);
+    
+	List<EventDetailsEntity> findByUserId(Long userId);
+}
