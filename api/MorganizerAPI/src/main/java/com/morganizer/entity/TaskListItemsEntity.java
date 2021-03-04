ffdf3cef8640a2fb9ddf64002466ae0fc05f2592
Satyen Verma <sverma44@asu.edu>
2021-03-03 22:08:58 -0700

    Task#41 API endpoint to add task items to list

--- api/MorganizerAPI/src/main/java/com/morganizer/entity/TaskListItemsEntity.java
+++ api/MorganizerAPI/src/main/java/com/morganizer/entity/TaskListItemsEntity.java
@@ -1,10 +1,25 @@
-package com.morganizer.dto;
+package com.morganizer.entity;
 
 import java.sql.Timestamp;
 
-public class TaskItemRequest {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity(name="todo_items")
+public class TaskListItemsEntity {
 	
-	private long categoryId;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)	
+	private int itemId;
+	
+	@ManyToOne
+	@JoinColumn(name="category_id",referencedColumnName="id")
+	private EventCategoriesEntity eventCategory;
+	//TODO: Add reference to calendar ID from calendar table
 	private int calendarId;
 	private String title;
 	private String itemDesc;
@@ -13,12 +28,16 @@ public class TaskItemRequest {
 	private String repeatType;
 	private Boolean complete;
 	private int[] assigneeId;
-	private int listId;
 	
-	public TaskItemRequest(long categoryId, int calendarId, String title, String itemDesc, Timestamp dueDate,
-			String url, String repeatType, Boolean complete, int[] assigneeId, int listId) {
+	@JoinColumn(name="list_id",referencedColumnName="id")
+	private TaskEntity task;
+
+	public TaskListItemsEntity(int itemId, EventCategoriesEntity eventCategory, int calendarId, String title,
+			String itemDesc, Timestamp dueDate, String url, String repeatType, Boolean complete, int[] assigneeId,
+			TaskEntity task) {
 		super();
-		this.categoryId = categoryId;
+		this.itemId = itemId;
+		this.eventCategory = eventCategory;
 		this.calendarId = calendarId;
 		this.title = title;
 		this.itemDesc = itemDesc;
@@ -27,67 +46,95 @@ public class TaskItemRequest {
 		this.repeatType = repeatType;
 		this.complete = complete;
 		this.assigneeId = assigneeId;
-		this.listId = listId;
+		this.task = task;
 	}
-	
-	public long getCategoryId() {
-		return categoryId;
+
+	public int getItemId() {
+		return itemId;
 	}
-	public void setCategoryId(long categoryId) {
-		this.categoryId = categoryId;
+
+	public void setItemId(int itemId) {
+		this.itemId = itemId;
 	}
+
+	public EventCategoriesEntity getEventCategory() {
+		return eventCategory;
+	}
+
+	public void setEventCategory(EventCategoriesEntity eventCategory) {
+		this.eventCategory = eventCategory;
+	}
+
 	public int getCalendarId() {
 		return calendarId;
 	}
+
 	public void setCalendarId(int calendarId) {
 		this.calendarId = calendarId;
 	}
+
 	public String getTitle() {
 		return title;
 	}
+
 	public void setTitle(String title) {
 		this.title = title;
 	}
+
 	public String getItemDesc() {
 		return itemDesc;
 	}
+
 	public void setItemDesc(String itemDesc) {
 		this.itemDesc = itemDesc;
 	}
+
 	public Timestamp getDueDate() {
 		return dueDate;
 	}
+
 	public void setDueDate(Timestamp dueDate) {
 		this.dueDate = dueDate;
 	}
+
 	public String getUrl() {
 		return url;
 	}
+
 	public void setUrl(String url) {
 		this.url = url;
 	}
+
 	public String getRepeatType() {
 		return repeatType;
 	}
+
 	public void setRepeatType(String repeatType) {
 		this.repeatType = repeatType;
 	}
+
 	public Boolean getComplete() {
 		return complete;
 	}
+
 	public void setComplete(Boolean complete) {
 		this.complete = complete;
 	}
+
 	public int[] getAssigneeId() {
 		return assigneeId;
 	}
+
 	public void setAssigneeId(int[] assigneeId) {
 		this.assigneeId = assigneeId;
 	}
-	public int getListId() {
-		return listId;
+
+	public TaskEntity getTask() {
+		return task;
 	}
-	public void setListId(int listId) {
-		this.listId = listId;
+
+	public void setTask(TaskEntity task) {
+		this.task = task;
 	}	
+
 }
