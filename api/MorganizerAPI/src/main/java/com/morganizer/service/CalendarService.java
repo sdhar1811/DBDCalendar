3984ef00b8c65f1560ca85fbe9a7bc187c9538ae
ddharne <ddharne@users.github.com>
2021-03-22 15:22:51 -0700

    Task#124: Created api to add new calendar

--- api/MorganizerAPI/src/main/java/com/morganizer/service/CalendarService.java
+++ api/MorganizerAPI/src/main/java/com/morganizer/service/CalendarService.java
@@ -6,25 +6,43 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.morganizer.dto.CalendarRequest;
 import com.morganizer.dto.CalendarResponse;
 import com.morganizer.entity.CalendarEntity;
+import com.morganizer.entity.UserDetailsEntity;
 import com.morganizer.repository.CalendarRepository;
+import com.morganizer.repository.UserDetailsRepository;
 
 @Service
 public class CalendarService {
-	
+
 	@Autowired
 	public CalendarRepository calendarRepository;
 
+	@Autowired
+	public UserDetailsRepository userRepo;
+
 	public List<CalendarResponse> fetchAll(Long userId) {
-		// TODO Auto-generated method stub
-		List<CalendarEntity> calendarList =  calendarRepository.findByUserId(userId);
-		
+		List<CalendarEntity> calendarList = calendarRepository.findByUserId(userId);
+
 		List<CalendarResponse> result = new ArrayList<>();
-		for(CalendarEntity calendar: calendarList) {
-			result.add(new CalendarResponse(calendar.getCalendarId(), calendar.getName(), calendar.getColor(), calendar.getUser().getId()));
+		for (CalendarEntity calendar : calendarList) {
+			result.add(new CalendarResponse(calendar.getCalendarId(), calendar.getName(), calendar.getColor(),
+					calendar.getUser().getId()));
 		}
 		return result;
 	}
-	
+
+	public List<CalendarResponse> saveCalendar(CalendarRequest calendarRequest) {
+		UserDetailsEntity user = userRepo.getOne(calendarRequest.getUserId());
+		CalendarEntity calendar = new CalendarEntity(calendarRequest.getName(), calendarRequest.getColor(), user);
+
+		if (calendarRequest.getCalendarId() != 0) {
+			calendar.setCalendarId(calendarRequest.getCalendarId());
+		}
+		calendarRepository.save(calendar);
+		return fetchAll(calendarRequest.getUserId());
+
+	}
+
 }
