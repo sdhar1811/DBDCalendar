5d5eae9e7cc1d81a4e8175e17fd6d3849cfa2ee3
AsmiGandhi <aagandh3@asu.edu>
2021-03-23 22:01:05 -0700

    Updated calendar n profile list on create event popup

--- ui/MorganizerUI/src/app/create-event/create-event.component.ts
+++ ui/MorganizerUI/src/app/create-event/create-event.component.ts
@@ -8,6 +8,9 @@ import { EventEmitter } from '@angular/core';
 import { EventService } from '../services/event.service';
 import { NgxMatDatetimePickerModule, NgxMatTimepickerModule, NgxMatDateFormats, NGX_MAT_DATE_FORMATS} from '@angular-material-components/datetime-picker';
 import { NgxMatMomentModule } from '@angular-material-components/moment-adapter';
+import { ProfileService } from '../services/profile.service';
+import { StoreService } from '../services/store.service';
+import { MyCalendarService } from '../services/mycalendar.service';
 
 
 export const DATETIME_WITHOUT_SECONDS_FORMAT = 'MM-DD-YYYY, hh:mm A';
@@ -35,16 +38,21 @@ export class CreateEventComponent implements OnInit {
   color: any;
   editFlag: boolean = false;
   calendarList = [];
+  assigneeList = [];
   public enableMeridian = true;
 
   constructor(
     private dialogRef: MatDialogRef<CreateEventComponent>,
     private eventService: EventService,
+    private profileService: ProfileService,
+    private storeService: StoreService,
+    private calendarService: MyCalendarService,
     @Inject(MAT_DIALOG_DATA) public data
   ) {}
 
   ngOnInit(): void {
-    this.fetchCalendars();
+    this.fetchMyCalendarList();
+    this.fetchAssigneeList();
     this.editFlag = this.data.title == null ? false : true;
     // this.color = this.data.color == null ? '#1e90ff' : this.data.color;
     // this.data.participant = [1];
@@ -68,12 +76,12 @@ export class CreateEventComponent implements OnInit {
   ];
 
   reminderLst = [
-    { title: '5 minutes', value: '5', id:   1 },
-    { title: '10 minutes', value: '10', id:   2   },
-    { title: '15 minutes', value: '15', id:   3   },
-    { title: '30 minutes', value: '30', id:   4 },
-    { title: '1 hour', value: '60', id:   5 },
-    { title: '2 hours', value: '120', id:   6 },
+    { title: '5 minutes', value: '5', id: 1 },
+    { title: '10 minutes', value: '10', id: 2 },
+    { title: '15 minutes', value: '15', id: 3 },
+    { title: '30 minutes', value: '30', id: 4 },
+    { title: '1 hour', value: '60', id: 5 },
+    { title: '2 hours', value: '120', id: 6 },
   ];
 
   createEvent(): void {
@@ -119,10 +127,42 @@ export class CreateEventComponent implements OnInit {
     this.close();
   }
 
-  fetchCalendars() {
-    this.calendarList.push({ name: 'Work', color: '#00ACC1', value: '' });
-    this.calendarList.push({ name: 'Personal', color: '#AB47BC', value: '' });
-    this.calendarList.push({ name: 'School', color: '#455A64', value: '' });
-    this.calendarList.push({ name: 'Medical', color: '#C0CA33', value: '' });
+  fetchMyCalendarList() {
+    this.calendarService
+      .getAllCalendars(this.storeService.loggedInUser?.id)
+      .subscribe(
+        (response) => {
+          if (response) {
+            console.log(response);
+            this.calendarList = response;
+          }
+        },
+        (error) => {
+          // this.loading = false;
+          //TODO:Handle API error
+        }
+      );
+    // this.this.calendarList.push({ name: 'Work', color: '#00ACC1', value: '' });
+    // this.calendarList.push({ name: 'Personal', color: '#AB47BC', value: '' });
+    // this.calendarList.push({ name: 'School', color: '#455A64', value: '' });
+    // this.calendarList.push({ name: 'Medical', color: '#C0CA33', value: '' });
   }
+
+  fetchAssigneeList() {
+    this.profileService
+      .getAllProfile(this.storeService.loggedInUser?.id)
+      .subscribe(
+        (response) => {
+          if (response) {
+            console.log(response);
+            this.assigneeList = response;
+          }
+        },
+        (error) => {
+          // this.loading = false;
+          //TODO:Handle API error
+        }
+      );
+  }
+
 }
