c41ddd9d67aae5d1c25161a29c6679885d20d12c
AsmiGandhi <aagandh3@asu.edu>
2021-03-24 18:06:06 -0700

    fix - ui- datetimepicker in create events

--- ui/MorganizerUI/src/app/create-event/create-event.component.ts
+++ ui/MorganizerUI/src/app/create-event/create-event.component.ts
@@ -40,6 +40,7 @@ export class CreateEventComponent implements OnInit {
   calendarList = [];
   assigneeList = [];
   public enableMeridian = true;
+  public defaultTime = [new Date().getHours, 0];
 
   constructor(
     private dialogRef: MatDialogRef<CreateEventComponent>,
@@ -88,7 +89,9 @@ export class CreateEventComponent implements OnInit {
     // this.data.color = this.color;
     console.log(JSON.stringify(this.data));
     console.log(this.data.reminderList);
-    this.data.color = this.calendarList.filter(calendar => calendar.calendarId == this.data.calendarId).map(calendar => calendar.color)[0];
+    this.data.color = this.calendarList
+      .filter((calendar) => calendar.calendarId == this.data.calendarId)
+      .map((calendar) => calendar.color)[0];
     console.log(this.data.color);
 
     this.eventService.addEvent(this.data).subscribe(
@@ -108,7 +111,9 @@ export class CreateEventComponent implements OnInit {
   editEvent(): void {
     //this.data.color = { primary: this.color, secondary: this.color };
     // this.data.color = this.color;
-    this.data.color = this.calendarList.filter(calendar => calendar.calendarId == this.data.calendarId).map(calendar => calendar.color)[0];
+    this.data.color = this.calendarList
+      .filter((calendar) => calendar.calendarId == this.data.calendarId)
+      .map((calendar) => calendar.color)[0];
     console.log(JSON.stringify(this.data));
     this.eventService.updateEvent(this.data).subscribe(
       (response) => {
@@ -166,5 +171,4 @@ export class CreateEventComponent implements OnInit {
         }
       );
   }
-
 }
