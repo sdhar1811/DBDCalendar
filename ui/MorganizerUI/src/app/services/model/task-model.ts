8255c6f70ce4dfcf84f12e2f00ed1649cb780d2c
Sharad Dhar <42695310+sdhar1811@users.noreply.github.com>
2021-03-30 13:19:41 -0700

    Task#131: updated mapping of assignee and calendar in todo list

--- ui/MorganizerUI/src/app/services/model/task-model.ts
+++ ui/MorganizerUI/src/app/services/model/task-model.ts
@@ -3,15 +3,10 @@ import { EventModel } from './event-model';
 
 export class TaskModel extends EventModel {
   id: any = undefined;
-  title: string;
-  description: string;
-  calendar: {};
-  color: string;
   complete = false;
   draggable = true;
   start: Date = startOfToday();
   dueDate: Date = undefined;
-  userId: number;
   todoListId: number;
-  assigneeList: number[] = [];
+  calendar = {};
 }
