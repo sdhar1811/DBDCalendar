948abd01183f3d0ea83bbc96f0db345c19b2a7bc
khushboogithub <khushboogupta1406@gmail.com>
2021-03-31 16:17:55 -0700

    Added Component for Calendar control

---
+++ ui/MorganizerUI/src/app/core/calendar-control/calendar-control.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-calendar-control',
+  templateUrl: './calendar-control.component.html',
+  styleUrls: ['./calendar-control.component.scss']
+})
+export class CalendarControlComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
