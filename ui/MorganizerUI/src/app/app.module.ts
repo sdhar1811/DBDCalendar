b50845247c02cea44755e155fa6ef57231375bfe
Sharad Dhar <42695310+sdhar1811@users.noreply.github.com>
2021-04-10 22:21:32 -0700

    (fix) added show more events pop-up

--- ui/MorganizerUI/src/app/app.module.ts
+++ ui/MorganizerUI/src/app/app.module.ts
@@ -69,6 +69,7 @@ import { DragAndDropModule } from 'angular-draggable-droppable';
 import { NgxMatMomentModule } from '@angular-material-components/moment-adapter';
 import { CalendarControlComponent } from './core/calendar-control/calendar-control.component';
 import { EventDetailsDialogComponent } from './core/home-screen/event-details-dialog/event-details-dialog.component';
+import { MoreEventsDialogComponent } from './more-events-dialog/more-events-dialog.component';
 
 @NgModule({
   declarations: [
@@ -93,6 +94,7 @@ import { EventDetailsDialogComponent } from './core/home-screen/event-details-di
     CustomDateFormat2,
     CalendarControlComponent,
     EventDetailsDialogComponent,
+    MoreEventsDialogComponent,
   ],
   imports: [
     BrowserModule,
