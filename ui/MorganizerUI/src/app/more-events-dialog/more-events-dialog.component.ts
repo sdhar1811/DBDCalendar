c092450eda2e9c781d1f5c9c4ede043b0312fde1
Sharad Dhar <42695310+sdhar1811@users.noreply.github.com>
2021-04-12 13:24:21 -0700

    (fix) long spanning events in show more events pop-up

--- ui/MorganizerUI/src/app/more-events-dialog/more-events-dialog.component.ts
+++ ui/MorganizerUI/src/app/more-events-dialog/more-events-dialog.component.ts
@@ -5,6 +5,7 @@ import {
   MatDialogRef,
   MAT_DIALOG_DATA,
 } from '@angular/material/dialog';
+import { differenceInCalendarDays } from 'date-fns';
 import { EventDetailsDialogComponent } from '../core/home-screen/event-details-dialog/event-details-dialog.component';
 import { StoreService } from '../services/store.service';
 
@@ -28,11 +29,14 @@ export class MoreEventsDialogComponent implements OnInit {
   ngOnInit(): void {
     const matDialogConfig: MatDialogConfig = new MatDialogConfig();
     const rect = this.data.refElement.nativeElement.getBoundingClientRect();
+    const dialogOffset = 15;
     matDialogConfig.position = {
-      left: `${rect.left}px`,
-      top: `${rect.top}px`,
+      left: Number(`${rect.left}`) - dialogOffset + 'px',
+      top: Number(`${rect.top}`) - dialogOffset + 'px',
     };
-    matDialogConfig.width = `${rect.width}px`;
+    const width = Number(`${rect.width}`) + dialogOffset * 3;
+
+    matDialogConfig.width = width + 'px';
     // matDialogConfig.height = `${rect.height}px`;
     this.matDialogRef.updateSize(matDialogConfig.width, matDialogConfig.height);
     this.matDialogRef.updatePosition(matDialogConfig.position);
@@ -45,5 +49,35 @@ export class MoreEventsDialogComponent implements OnInit {
   }
   dragEnd(event) {
     this.close();
+  }
+  isLongEvent(event) {
+    if (differenceInCalendarDays(event.end, event.start) > 0) {
+      return true;
+    }
+    return false;
+  }
+  checkIfFirstCell(event, currentDate) {
+    if (differenceInCalendarDays(currentDate, event.start) == 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  checkIfLastCell(event, currentDate) {
+    if (differenceInCalendarDays(currentDate, event.end) == 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  checkIfMiddleCell(event, currentDate) {
+    if (
+      !this.checkIfLastCell(event, currentDate) &&
+      !this.checkIfFirstCell(event, currentDate)
+    ) {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
